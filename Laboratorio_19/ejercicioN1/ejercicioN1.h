eb5d273c42149caef8e30748e6786032205f1b3d
JHONATAN BILBAO USCCA GIRALDO <104391558+JhonatanUscca@users.noreply.github.com>
2022-08-09 23:37:01 -0500

    Update ejercicioN1.h

--- Laboratorio_19/ejercicioN1/ejercicioN1.h
+++ Laboratorio_19/ejercicioN1/ejercicioN1.h
@@ -1,47 +1,55 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
-#include <vector>
 #include <iterator>
-#include <random>
- 
 using namespace std;
  
-class Elementos {
-    public:
-    void operator()(vector<int> &vec) {  
-        sort(vec.begin(),vec.begin());
-   
-    }
  
-    vector<int> operator()(vector<int> &vecA, vector<int> &vecB) {
-        vector<int> vecResultado;
+class RegresionLineal {
+public:
+    vector<double> operator()(vector<double> &vecX, vector<double> &vecY) {
+        vector<double> xCuadado;
+        vector<double> xy;
+        vector<double> resultado;
+        double sumaX = 0.0, sumaY = 0.0, sumaXCuadado = 0.0, sumaXY = 0.0, mediaX = 0.0, mediaY = 0.0;
+        double contador = 0.0;
+ 
+        double a , b , c;
+ 
+       vecY.push_back(2);   vecX.push_back(1);
+       vecY.push_back(3);   vecX.push_back(3);
+       vecY.push_back(2.4); vecX.push_back(4);
+       vecY.push_back(2);   vecX.push_back(2);
+       vecY.push_back(2);   vecX.push_back(1);
+       vecY.push_back(3.5); vecX.push_back(7);
        
-        mt19937 gen(3445);
-        uniform_int_distribution<> dist(1,30);
- 
-        for (int i = 0; i < 20; ++i) {
-            vecA.push_back(dist(gen));
+       
+       
+        for (size_t i = 0; i < vecX.size(); ++i) {
+            xCuadado.push_back(vecX[i]*vecX[i]);
+            xy.push_back(vecX[i]*vecY[i]);
         }
-         
-        mt19937 gena(45345);
-        uniform_int_distribution<> dists(1,30);
  
-        for (int i = 0; i < 20; ++i) {
-            vecB.push_back(dists(gena));
+        for (size_t i = 0; i < vecX.size(); ++i) {
+            sumaX = sumaX+vecX[i];
+            sumaY = sumaY+vecY[i];
+            sumaXCuadado = sumaXCuadado+xCuadado[i];
+            sumaXY = sumaXY+xy[i];
+            contador++;
         }
        
-     
-        operator()(vecA);
-        operator()(vecB);
  
-        for (size_t i = 0; i < vecA.size(); ++i) {
-            if(vecA[i] < vecB[i] ) {
-                vecResultado.push_back(vecA[i]);
-            }
-        }
+        mediaX = sumaX/contador;
+        mediaY = sumaY/contador;
  
-        return vecResultado;
+ 
+        b = (sumaXY-((contador)*(mediaX)*(mediaY)))/(sumaXCuadado-((contador)*(mediaX)*(mediaX)));
+ 
+ 
+        a = mediaY-((b)*(mediaY));
+ 
+        resultado.push_back(a);
+        resultado.push_back(b);
+       
+        return resultado;
     }
-   
 };
