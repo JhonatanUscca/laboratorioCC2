490630170d4ffbe7f72516fe6a8d7562eade2e00
JHONATAN BILBAO USCCA GIRALDO <104391558+JhonatanUscca@users.noreply.github.com>
2022-08-14 22:34:41 -0500

    Create ejercicioN5.h

---
+++ Laboratorio_20/ejercicioN5/ejercicioN5.h
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+#include <memor>
+//No implementan operadores flecha -> ni asterisco *,
+ 
+class C1
+{
+private:
+    std::shared_ptr<double> d;
+public:
+    C1(std::shared_ptr<double> value) : d(value) {}
+    virtual ~C1() { cout << "\nC1 destructor\n"; }
+    void print() const { cout << "Valor " << *d << endl; }
+};
+ 
+ 
+class C2
+{
+private:
+    std::shared_ptr<double> d;
+public:
+    C2(std::shared_ptr<double> value) : d(value) {}
+    virtual ~C2() { cout << "\nC2 destructor\n"; }
+    void print() const { cout << "Valor " << *d << endl; }
+};
